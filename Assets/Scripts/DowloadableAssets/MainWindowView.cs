6fcdd98ed81b078f89bd43721a7cc941373d64c0
Brundewald <77693457+Brundewald@users.noreply.github.com>
2021-12-13 16:52:45 +0300

    Добавил addressables.
    Теперь при нажатии кнопки LoadObject появится кнопка SpawnObject.

--- Assets/Scripts/DowloadableAssets/MainWindowView.cs
+++ Assets/Scripts/DowloadableAssets/MainWindowView.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.DowloadableAssets
@@ -10,23 +10,33 @@ namespace Assets.Scripts.DowloadableAssets
     public class MainWindowView: AssetBundleViewBase
     {
         [SerializeField] private Button _loadAssetButton;
+        [SerializeField] private AssetReference _loadPrefab;
+        [SerializeField] private RectTransform _mountSpawnTransform;
+        private List<AsyncOperationHandle<GameObject>> _addressablePrefabs = new List<AsyncOperationHandle<GameObject>>();
 
 
         private void Start()
         {
-            _loadAssetButton.onClick.AddListener(LoadAssetsAsync);
+            _loadAssetButton.onClick.AddListener(LoadAssetsAndSpawn);
         }
 
         private void OnDestroy()
         {
             _loadAssetButton.onClick.RemoveAllListeners();
+            foreach (var addressablePrefab in _addressablePrefabs)
+            {
+                Addressables.ReleaseInstance(addressablePrefab);
+            }
+            _addressablePrefabs.Clear();
         }
 
-        private void LoadAssetsAsync()
+        private void LoadAssetsAndSpawn()
         {
             _loadAssetButton.interactable = false;
             Debug.Log(Thread.CurrentThread.ManagedThreadId);
             DownloadAndSetAssetBundleAsync();
+            var addressablePrefab = Addressables.InstantiateAsync(_loadPrefab, _mountSpawnTransform);
+            _addressablePrefabs.Add(addressablePrefab);
         }
     }
 }
